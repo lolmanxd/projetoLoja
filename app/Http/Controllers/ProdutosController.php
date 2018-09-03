79ed67e7575d744540a1b7550f299f31a7cb6bdf
MárioM <mario_marques185@hotmail.com>
2018-09-03 04:02:58 -0300

    projeto inicial

---
+++ app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutosController extends Controller
+{
+    //
+
+    public function index(){
+
+    }
+
+    public function add(){
+
+    }
+
+    public function edit(){
+        
+    }
+
+}
